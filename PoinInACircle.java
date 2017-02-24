30d143da10c5ffb55fa292b8d2fff274f1231a19
Akassh Shah <akassh.shah77@gmail.com>
2017-02-24 16:11:23 -0500

    Made changes so it's not limited to just radius 1

--- PoinInACircle.java
+++ PoinInACircle.java
@@ -1,21 +1,25 @@
 import java.util.*;
 import java.io.*;
 public class PoinInACircle{
-   public static double randNumber(){
-      double rand=(Math.random()*2)-1;//creates a double from -1 to 1
+   public static double randNumber(double r){
+      double rand=(Math.random()*2*r)-r;//creates a double from -r(inclusive) to r(exclusive)
       return(rand);
    }
    public static void main(String[] args){
-      System.out.println("There is a circle with the equation x^2+y^2=1. This program will generate a random point and see if it is within this circle.");
-      System.out.println("The probability that the point is withing the circle is(in percent) "+(100*(Math.PI/4)));
+      double radius=1;
+      Scanner scan=new Scanner(System.in);
+      System.out.println("Enter radius of circle");
+      radius=scan.nextDouble();
+      System.out.println("There is a circle with the equation x^2+y^2="+Math.pow(radius,2)+". This program will generate a random point that lies in a square that has a length of "+(2*radius)+" and see if it is within this circle.");
+      System.out.println("The probability that the point is withing the circle is(in percent) "+(100*((Math.pow(radius,2)*Math.PI)/(4*Math.pow(radius,2)))));
       int count=0;
       double x,y;
       int repeat=100;
       for(int i=0;i<repeat;i++){
-         x=randNumber();
-         y=randNumber();
+         x=randNumber(radius);
+         y=randNumber(radius);
          //System.out.println("The point in consideration is ("+x+", "+y+")");
-         if(Math.pow(x,2)+Math.pow(y,2)<=1){
+         if(Math.pow(x,2)+Math.pow(y,2)<=Math.pow(radius,2)){
             count++;
          }
       }
